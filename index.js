3c4a60208851362d4741c851850b0b112fca26c9
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-10-04 18:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var name = "Joe"
-var height = "74"
-if (name === "Susan") {
-  console.log("The name is correct")
-}else {
-  console.log("Expected the name to be Susan")
+var name = "Susan"
+var height = "70"
+if (name === "Susan" & height === "70") {
+  console.log("You got both name and height")
 }
 
 // Don't worry about this
